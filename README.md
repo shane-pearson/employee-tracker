df028ca6f00fbdaf515f4c534067ec42cf6b707b
Pearson <spearson@lwsd.org>
2023-02-09 12:46:33 -0800

    made a few updates and created readme

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# employee-tracker
+# Description
+
+This project was created to show the power of using node and inquirer to combine what we are learning with SQL. We are dynamically displaying, updating, and creating tables through node. These changes are reflected in our database. 
+
+https://github.com/shane-pearson/employee-tracker
+
+## Usage
+
+This project would be something great for anyone to build upon. You can easily create tables and columns that you desire, adn easily update them through a series of prompts. 
+
+### Installation
+
+To install I started by creating the files I would need and downloading all my dependencies. Next I Built out my schema and seeds files and ran them through sql. Finally I began work on my server file. I required what I was using. Added my prompts, and create functions to respond to to whatever choices were selected. 
+
+#### Credits
+
+Various tutorials. Classroom instruction, office hours, tutoring, and askbcs. 
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const mysql = require('mysql2');
+require('dotenv').config();
 
 
 const db = mysql.createConnection(
@@ -17,7 +18,7 @@ db.connect(err => {
     if (err) throw err
 })
 
-// function promptUser() {
+function promptUser() {
     inquirer.prompt([
       {
         type: 'list',
@@ -43,9 +44,10 @@ db.connect(err => {
         } else {
           quit()
         }
+        
     }) 
 
-// };
+};
 
 
 viewEmployees = () => {
@@ -55,39 +57,154 @@ viewEmployees = () => {
   promptUser()
 }
 
-addEmployee = () => {
 
-    
-}
+addEmployee = () => {
+  inquirer
+  .prompt([
+    {
+      name: `first_name`,
+      type: `input`,
+      message: `What is the employee's first name?`,
+      validate: (firstNameInput) => {
+        if (firstNameInput) {
+          return true;
+        } else {
+          console.log("You need to enter the employee's first name!");
+          return false;
+        }
+      },
+    },
+    {
+      name: `last_name`,
+      type: `input`,
+      message: `What is the employee's last name?`,
+      validate: (lastNameInput) => {
+        if (lastNameInput) {
+          return true;
+        } else {
+          console.log("You need to enter the employee's last name!");
+          return false;
+        }
+      },
+    },
+    {
+      name: `role_id`,
+      type: `input`,
+      message: `What is the employee's role id?`,
+      validate: (employeeRoleIdInput) => {
+        if (employeeRoleIdInput) {
+          return true;
+        } else {
+          console.log("You need to the employee's role!");
+          return false;
+        }
+      },
+    },
+    {
+      name: `manager_id`,
+      type: `input`,
+      message: `What is the employee's manager's id`,
+      validate: (managerNameIdInput) => {
+        if (managerNameIdInput) {
+          return true;
+        } else {
+          console.log("You need the employee's manager!");
+          return false;
+        }
+      },
+    },
+  ])
+  .then((answers) => {
+    console.log(answers);
+    const sql = "INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES (?,?,?,?)";
+    const params =[answers.first_name, answers.last_name, answers.role_id, answers.manager_id];
+
+    db.query(sql, params, function (err, results) {
+      if (err) throw err;
+      console.log(results);
+      promptUser();
+    });
+  });
+};
 
 updateEmployee = () => {
-
-    
-}
+ 
+  
+};
 
 viewRole = () => {
-
+  db.query(`SELECT * FROM role`, (err, res) => {
+    console.table(res);
+})
+promptUser()
     
 }
 
 addRole = () => {
+  db.query("SELECT role.title AS Title, role.salary AS Salary FROM role",   function(err, res) {
+    inquirer.prompt([
+        {
+          name: "Title",
+          type: "input",
+          message: "What is the roles Title?"
+        },
+        {
+          name: "Salary",
+          type: "input",
+          message: "What is the Salary?"
 
-    
-}
+        } 
+    ]).then(function(res) {
+        db.query(
+            "INSERT INTO role SET ?",
+            {
+              title: res.Title,
+              salary: res.Salary,
+            },
+            function(err) {
+                if (err) throw err
+                console.table(res);
+                promptUser();
+            }
+        )
+      });
+    });
+  }
 
 viewDepartment = () => {
-
+  db.query(`SELECT * FROM department`, (err, res) => {
+    console.table(res);
+})
+promptUser()
     
 }
 
 addDepartment = () => {
+  let questions = [
+    {
+      type: "input",
+      name: "name",
+      message: "what is the new department name?"
+    }
+  ];
 
+  inquirer.prompt(questions)
+  .then(response => {
+    const query = `INSERT INTO department (name) VALUES (?)`;
+    db.query(query, [response.name], (err, res) => {
+      if (err) throw err;
+      console.log(`Successfully inserted ${response.name} department at id ${res.insertId}`);
+      promptUser();
+    });
+  })
+  .catch(err => {
+    console.error(err);
+  });
     
 }
 
-quit = () => {
-
-    
+function quit () {
+  prompt.ui.close();
 }
 
 
